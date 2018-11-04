e53ae0b4e4ffe0ae9b290f752950fc2fc3455902
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-11-04 13:08:28 +0200

    add the android folder that contains everything related to the android app backend

---
+++ android/APIs/saveDeviceToken.php
@@ -0,0 +1,9 @@
+<?php
+
+/* this file will be called from the android app to store the device token 
+  in the db */
+
+include_once '../../BLL/android/device_token.php';
+
+$device_token = new device_token();
+$device_token->store_device_token($_GET['deviceToken']);

e4c7c588c9cc12f55d7219fa6fdbab30f079196e
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-11-15 08:56:53 +0200

    i've organized the comments

--- PRL/events_insert.php
+++ PRL/events_insert.php
@@ -3,13 +3,24 @@
 include_once 'include/check_session.php';
 include_once '../BLL/events.php';
 
-//bellow i'll check if the event appointment is set in the event appointemnt textbox, to view it in the live screen instead of the event time.
-if (isset($_POST['event_appointment']) && !empty(trim($_POST['event_appointment']))) {
+//bellow i'll check if the event appointment is set in the event appointemnt 
+//textbox, to view it in the live screen instead of the event time.
+if (isset($_POST['event_appointment']) &&
+        !empty(trim($_POST['event_appointment']))) {
     $event_appointment = $_POST['event_appointment'];
-    $event_time = date("H:i:s", strtotime($_POST['time']) + 1); //here i set the value of the time from the form to the event_time variable and increase it with one second so it can come on the live screen after the event that it should come after it
-} else {//here when the event appointment is empty
-    $event_appointment = $_POST['event_appointment']; //here the event appointment will be empty
-    $event_time = $_POST['time']; //here i set the value of the time from the form to the event_time variable as it's, because the events appointment has not set, so i won't need the increase the event time by one second
+    //here i set the value of the time from the form to the event_time variable 
+    //and increase it with one second so it can come on the live screen after 
+    //the event that it should come after it
+    $event_time = date("H:i:s", strtotime($_POST['time']) + 1);
+}
+//here when the event appointment is empty
+else {
+    //here the event appointment will be empty
+    $event_appointment = $_POST['event_appointment'];
+    //here i set the value of the time from the form to the event_time variable 
+    //as it's, because the events appointment has not set, so i won't need the 
+    //increase the event time by one second
+    $event_time = $_POST['time'];
 }
 
 if (isset($_POST['event_status'])) {
@@ -17,11 +28,14 @@ if (isset($_POST['event_status'])) {
 } else {
     $event_status = 0;
 }
-if ((isset($_POST['hall'])) && !empty(trim($_POST['hall']))) {//here i check if the hall has been choosed
+//here i check if the hall has been choosed
+if ((isset($_POST['hall'])) && !empty(trim($_POST['hall']))) {
     $hall_id = $_POST['hall'];
     $event_place = '';
 } elseif (isset($_POST['event_place_textbox'])) {
-    if (!empty(trim($_POST['event_place_textbox']))) {//here i check if the hall hasn't been choosen, instead the text box filled with the place of the event
+    //here i check if the hall hasn't been choosen, instead the text box filled 
+    //with the place of the event
+    if (!empty(trim($_POST['event_place_textbox']))) {
         $event_place = $_POST['event_place_textbox'];
         $hall_id = 0;
     } else {
@@ -29,22 +43,42 @@ if ((isset($_POST['hall'])) && !empty(trim($_POST['hall']))) {//here i check if 
         $hall_id = 0;
     }
 }
-//bellow i'll check if the user choosed the event entity name (like committee) from the dropdown menu or typed it's name in the textbox
-if (isset($_POST['committee']) && !empty(trim($_POST['committee']))) {//here i check the event entity has been choosed form the dropdown menu
-    $event_entity = $_POST['committee']; //here i choose the event entity name that choosen from the dropdown to be inserted
-    $event_entity_name = ''; //here i make the event entity name empty, because the name choosed from the dropdown
+//bellow i'll check if the user choosed the event entity name (like committee) 
+//from the dropdown menu or typed it's name in the textbox
+//here i check the event entity has been choosed form the dropdown menu
+if (isset($_POST['committee']) && !empty(trim($_POST['committee']))) {
+    //here i choose the event entity name that choosen from the dropdown to be 
+    //inserted
+    $event_entity = $_POST['committee'];
+    //here i make the event entity name empty, because the name choosed from the 
+    //dropdown
+    $event_entity_name = '';
 } else {
-    $event_entity_name = $_POST['event_entity_name']; //here i choose the event entity name that typed in the event entity textbox to be inserted
-    if ($_SESSION['directorate'] == 2) {//here i check if the directorate of the user is legislate affiars with value 2, to store 2 in the committee id
-        $event_entity = 2; //here i make the event entity to 2, because the name typed in the textbox
-    } elseif ($_SESSION['directorate'] == 3) {//here i check if the directorate of the user is general affairs with value 3, to store 3 in the committee id
-        $event_entity = 3; //here i make the event entity to 3, because the name typed in the textbox
-    } elseif ($_SESSION['directorate'] == 4) {//here i check if the directorate of the user is blocs with value 4, to store 4 in the entity event id
-        $event_entity = 4; //here i make the event entity to 4, because the name typed in the textbox
+    //here i choose the event entity name that typed in the event entity textbox 
+    //to be inserted
+    $event_entity_name = $_POST['event_entity_name'];
+    //here i check if the directorate of the user is legislate affiars with 
+    //value 2, to store 2 in the committee id
+    if ($_SESSION['directorate'] == 2) {
+        //here i make the event entity to 2, because the name typed in the textbox
+        $event_entity = 2;
+        //here i check if the directorate of the user is general affairs with 
+        //value 3, to store 3 in the committee id
+    } elseif ($_SESSION['directorate'] == 3) {
+        //here i make the event entity to 3, because the name typed in the textbox
+        $event_entity = 3;
+        //here i check if the directorate of the user is blocs with value 4, to 
+        //store 4 in the entity event id
+    } elseif ($_SESSION['directorate'] == 4) {
+        //here i make the event entity to 4, because the name typed in the textbox
+        $event_entity = 4;
     }
 }
 $event1 = new events();
-$event1->insert_event($event_entity, $event_entity_name, $event_time, $event_appointment, $hall_id, $event_place, nl2br($_POST['subject']), $_POST['event_date'], $event_status, $_SESSION['directorate'], $_SESSION['user_id']);
+$event1->insert_event($event_entity, $event_entity_name, $event_time
+        , $event_appointment, $hall_id, $event_place, nl2br($_POST['subject'])
+        , $_POST['event_date'], $event_status, $_SESSION['directorate']
+        , $_SESSION['user_id']);
 
 /* this inclusion for the eventNotificatoin file to send notifications when the 
   event inserted in the db */

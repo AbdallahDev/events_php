83106a8b726d7a8d6dd5cf52f2cf9ec9694c711b
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-11-19 13:20:29 +0200

    i've created the event_entity_category class with the function event_entity_category_get_all to select all the event entity categories

---
+++ BLL/event_entity_category.php
@@ -0,0 +1,12 @@
+<?php
+
+include_once '../DAL/my_db.php';
+
+//this class to manage the event_entity_category table in the database
+class event_entity_category extends my_db {
+
+    public function event_entity_category_get_all() {
+        return $this->get_all_data('SELECT * FROM `event_entity_category`');
+    }
+
+}

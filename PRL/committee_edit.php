dd5d724c474d72933970434ca898a28f159dfef7
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-11-21 09:46:16 +0200

    i've edited the committee_edit file to view the event entity categories in the drop down list

--- PRL/committee_edit.php
+++ PRL/committee_edit.php
@@ -67,27 +67,35 @@ $row_committee = $rs_committee->fetch_assoc();
                 <form class="w3-container w3-card-4 w3-padding-16 w3-white" action="committee_edit_ctl.php" method="post">
                     <input type = "hidden" name = "committee_id" value = "<?php echo $row_committee['committee_id'] ?>" class="w3-input w3-border">
 
+                    <!--this div for the event entity categories drop down list -->
                     <div class="w3-section">
-                        <label><?php
-                            //bellow i'll set the the input field label based on the user directorate
-                            if (($_SESSION['directorate'] == 2) || ($_SESSION['user_type'] == 0)) {//here i check if the directorate is legislative affairs with the id 2, or the user type is super admin
-                                echo 'اسم اللجنة';
-                            } elseif ($_SESSION['directorate'] == 3) {//here i check if the directorate is foreign affairs with the id 3
-                                echo "اسم اللجنة الدبلوماسية";
-                            } elseif ($_SESSION['directorate'] == 4) {//here i check if the directorate is blocs with the id 4
-                                echo "اسم الكتلة / الائتلاف";
-                            }
-                            ?></label>
-                        <input type="text" name="committee_name" <?php
-                        //bellow i'll set the placeholder value based on the user directorate
-                        if (($_SESSION['directorate'] == 2) || ($_SESSION['user_type'] == 0)) {//here i check if the directorate is legislative affairs with the id 2, or the user type is super admin
-                            ?> placeholder = "اسم اللجنة" <?php
-                               } elseif ($_SESSION['directorate'] == 3) {//here i check if the directorate is foreign affairs with the id 3
-                                   ?> placeholder = "اسم اللجنة الدبلوماسية" <?php
-                               } elseif ($_SESSION['directorate'] == 4) {//here i check if the directorate is blocs with the id 4
-                                   ?> placeholder = "اسم الكتلة / الائتلاف" <?php
-                               }
-                               ?> class="w3-input w3-border right-dir" value = "<?php echo $row_committee['committee_name'] ?>">
+                        <select class="w3-input w3-border right-dir" name="event_entity_category_id">
+                            <option value="">فئة جهة النشاط</option>
+                            <?PHP
+                            //bellow i'll view all the event entity catigories
+                            //
+                            //here is the php code to view the event entity 
+                            //categories in the drop down list
+                            include_once '../BLL/event_entity_category.php';
+                            $event_entity_category = new event_entity_category();
+                            $rs_event_entity_category = $event_entity_category->event_entity_category_get_all();
+                            while ($row_event_entity_category = $rs_event_entity_category->fetch_assoc()) {
+                                ?>
+                                <option value="
+                                        <?php echo $row_event_entity_category['event_entity_category_id']; ?>">
+                                            <?php echo $row_event_entity_category['event_entity_category_name']; ?>
+                                </option>
+                            <?php }
+                            ?>
+                        </select>
+                    </div>
+
+                    <!--this is the event entity name text box-->
+                    <div class="w3-section">
+                        <input type="text" name="committee_name" 
+                               placeholder = "جهة النشاط" 
+                               class="w3-input w3-border right-dir" 
+                               value = "<?php echo $row_committee['committee_name'] ?>">
                     </div>
                     <button class="w3-button w3-right w3-theme" type="submit" name="edit">تعديل</button>
                 </form>

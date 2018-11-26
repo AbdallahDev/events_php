e0148f0d8f869d0dedbfd11b4438c2cb89cad4da
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-11-26 13:37:54 +0200

    i've created a function to get the event id after it has been inserted

--- BLL/events.php
+++ BLL/events.php
@@ -14,6 +14,12 @@ class events extends my_db {
             , &$subject, &$event_date, &$hall_id, &$event_place, &$user_id, &$event_status));
     }
 
+    //this function get the id of the event after it has been inserted, to take it and inserted again
+    //in the event_event_entity table with the related even entities
+    function event_id() {
+        return $this->get_all_data("SELECT MAX(events.id) FROM `events`");
+    }
+
     function update_event($event_entity, $event_entity_name, $time, $event_appointment, $hall_id, $event_place, $subject, $event_date, $event_status, $event_edit_date, $directorate_id, $user_id_edit, $id) {
         $this->mod_data('update events set committee_id = ?, event_entity_name = ?, time = ?, event_appointment = ?, hall_id = ?, event_place = ?, subject = ?, event_date = ?, event_status = ?, event_edit_date = ?, directorate_id = ?, user_id_edit = ? where id = ?', 'isssisssisiii', array(&$event_entity, &$event_entity_name, &$time, &$event_appointment, &$hall_id, &$event_place, &$subject, &$event_date, &$event_status, &$event_edit_date, &$directorate_id, &$user_id_edit, &$id));
     }

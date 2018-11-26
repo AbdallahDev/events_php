7f2b474e36c12411b53aa00ddfad879f047332af
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-11-26 14:54:41 +0200

    i've changed the db row object name to be more convenient

--- PRL/events_get_preview_current_future.php
+++ PRL/events_get_preview_current_future.php
@@ -47,46 +47,46 @@ if ($events_rs->num_rows != 0) {
             </thead>
             <tbody>
                 <?php
-                while ($row = $events_rs->fetch_assoc()) {
+                while ($events_row = $events_rs->fetch_assoc()) {
                     ?>
                     <tr>
                         <td data-label="النشاط"><?php
-                            if ($row['committee_id'] > 4) {//here i check if the committee id is greater that 4, because 4 is the last id for the empty committees, and because that means that the event entity name is not choossed using the event entity dropdown menu saved using the event entity textbox
-                                echo $row['committee_name'];
-                            } elseif ($row['event_entity_name'] != '') {
-                                echo $row['event_entity_name'];
+                            if ($events_row['committee_id'] > 4) {//here i check if the committee id is greater that 4, because 4 is the last id for the empty committees, and because that means that the event entity name is not choossed using the event entity dropdown menu saved using the event entity textbox
+                                echo $events_row['committee_name'];
+                            } elseif ($events_row['event_entity_name'] != '') {
+                                echo $events_row['event_entity_name'];
                             } else {
                                 echo 'ـــــــــــــــ';
                             }
                             ?></td>
-                        <td data-label="الوقت"><?php echo date('h:i A', strtotime($row['time'])); ?></td>
+                        <td data-label="الوقت"><?php echo date('h:i A', strtotime($events_row['time'])); ?></td>
                         <td data-label="المكان" dir="rtl"><?php
-                            if ($row['hall_name'] != '') {
-                                echo $row['hall_name'];
-                            } elseif ($row['event_place'] != '') {
-                                echo $row['event_place'];
+                            if ($events_row['hall_name'] != '') {
+                                echo $events_row['hall_name'];
+                            } elseif ($events_row['event_place'] != '') {
+                                echo $events_row['event_place'];
                             } else {
                                 echo 'ـــــــــــــــ';
                             }
                             ?>
                         </td>
-                        <td data-label="الموضوع"><?php echo $row['subject']; ?></td>
-                        <td data-label="تاريخ النشاط"><?php echo $row['event_date']; ?></td>
+                        <td data-label="الموضوع"><?php echo $events_row['subject']; ?></td>
+                        <td data-label="تاريخ النشاط"><?php echo $events_row['event_date']; ?></td>
                         <!--here i print the date of the event insertion-->
                         <td data-label="تاريخ الانشاء">
                             <?php
-                            echo date('Y-m-d / h:i A', strtotime($row['event_insertion_date']));
+                            echo date('Y-m-d / h:i A', strtotime($events_row['event_insertion_date']));
                             ?>
                         </td>
                         <!--here i print the name of the user who inserted the event-->
-                        <td data-label="اسم المستخدم"><?php echo $row['name']; ?></td>
+                        <td data-label="اسم المستخدم"><?php echo $events_row['name']; ?></td>
                         <!--here i print the editing date of the event-->
                         <td data-label="تاريخ التعديل">
                             <?php
                             //here check if the event date of editing is grater than cuurent date
                             //to print the editing date
-                            if (date('Y-m-d', strtotime($row['event_edit_date'])) >= date('Y-m-d')) {
-                                echo date('Y-m-d / h:i A', strtotime($row['event_edit_date']));
+                            if (date('Y-m-d', strtotime($events_row['event_edit_date'])) >= date('Y-m-d')) {
+                                echo date('Y-m-d / h:i A', strtotime($events_row['event_edit_date']));
                             }
                             //here if the editing date is less than the current date
                             //a line will printed
@@ -98,7 +98,7 @@ if ($events_rs->num_rows != 0) {
                         <!--here i print the name of the user who last edited the event-->
                         <td data-label="اسم المستخدم">
                             <?php
-                            $rs_user = $user->get_user($row['user_id_edit']);
+                            $rs_user = $user->get_user($events_row['user_id_edit']);
                             $row_user = $rs_user->fetch_assoc();
                             //here print the username if there is one edited the event
                             if ($rs_user->num_rows != 0) {
@@ -111,7 +111,7 @@ if ($events_rs->num_rows != 0) {
                             ?>
                         </td>
                         <?php
-                        if (($row['event_status'] == 1) and $row['event_date'] == date('Y-m-d') and ( date('H:i') <= $row_event_max_time['time'])) {//here i check if the event status is enabled with value 1 and within the current date and it's time is within the max events time plus one hour
+                        if (($events_row['event_status'] == 1) and $events_row['event_date'] == date('Y-m-d') and ( date('H:i') <= $row_event_max_time['time'])) {//here i check if the event status is enabled with value 1 and within the current date and it's time is within the max events time plus one hour
                             ?>
                             <td data-label="الحالة" style="color: white; background-color: red">منشور</td>
                             <?php
@@ -123,8 +123,8 @@ if ($events_rs->num_rows != 0) {
                         //check if the user is regular with usertype value = 2 to view the edit and delte tabs. while all other user types they will be hidden
                         if ($_SESSION['user_type'] == 2) {
                             ?>
-                            <td data-label="تعديل"><a href="events_edit_page.php?id=<?php echo $row['id']; ?>">تعديل</a></td>
-                            <td data-label="حذف"><a href="events_delete.php?id=<?php echo $row['id']; ?>" onclick="return confirm('هل انت متأكد من الحذف');">حذف</a></td>
+                            <td data-label="تعديل"><a href="events_edit_page.php?id=<?php echo $events_row['id']; ?>">تعديل</a></td>
+                            <td data-label="حذف"><a href="events_delete.php?id=<?php echo $events_row['id']; ?>" onclick="return confirm('هل انت متأكد من الحذف');">حذف</a></td>
                             <?php
                         }
                         ?>

a9bf704a82c7ac77d731642f8540fc04df0c654d
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-11-26 15:21:42 +0200

    update the event_event_entity_name_get function to get the event entity name properly

--- BLL/event_event_entity.php
+++ BLL/event_event_entity.php
@@ -10,9 +10,11 @@ class event_event_entity extends my_db {
         return $this->mod_data("INSERT INTO `event_event_entity` (`event_id`, `event_entity_id`) VALUES (?, ?)", 'ii', array(&$event_id, &$event_entity_id));
     }
 
-    //this function get the event entity name for the event with the deleverd event id.
+    //this function get the event entity name for the event with the designated event id.
     function event_event_entity_name_get($event_id) {
-        $query = "SELECT event_event_entity.event_entity_id FROM `event_event_entity` WHERE event_event_entity.event_id = ?";
+        $query = "SELECT committees.committee_name FROM committees "
+                . "INNER JOIN event_event_entity ON event_event_entity.event_entity_id = committees.committee_id "
+                . "INNER JOIN events on events.id = event_event_entity.event_id WHERE events.id = ?";
         return $this->mod_data($query, 'i', array(&$event_id));
     }
 

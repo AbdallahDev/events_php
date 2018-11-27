1ec51ce37f392a7c90a22ce94cbd1260ac14c6bd
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-11-27 14:01:33 +0200

    created the function that get the event entity id

--- BLL/event_event_entity.php
+++ BLL/event_event_entity.php
@@ -9,5 +9,10 @@ class event_event_entity extends my_db {
     function event_event_entity_insert($event_id, $event_entity_id) {
         return $this->mod_data("INSERT INTO `event_event_entity` (`event_id`, `event_entity_id`) VALUES (?, ?)", 'ii', array(&$event_id, &$event_entity_id));
     }
+    
+    function entity_id_get($event_id) {
+        $query = "SELECT event_entity_id FROM `event_event_entity` WHERE event_id = ?";
+        return $this->get_data($query, 'i', array(&$event_id));
+    }
 
 }

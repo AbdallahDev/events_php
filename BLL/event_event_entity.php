1f32c30a2097efa02ece8e92cda8d4f7ed63989b
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-11-29 09:41:38 +0200

    created function to clear all the event entities related to a specific event

--- BLL/event_event_entity.php
+++ BLL/event_event_entity.php
@@ -9,10 +9,17 @@ class event_event_entity extends my_db {
     function event_event_entity_insert($event_id, $event_entity_id) {
         return $this->mod_data("INSERT INTO `event_event_entity` (`event_id`, `event_entity_id`) VALUES (?, ?)", 'ii', array(&$event_id, &$event_entity_id));
     }
-    
+
     function entity_id_get($event_id) {
         $query = "SELECT event_entity_id FROM `event_event_entity` WHERE event_id = ?";
         return $this->get_data($query, 'i', array(&$event_id));
+    }
+
+    function event_event_entity_deletion($event_id) {
+        $query = 'DELETE FROM event_event_entity WHERE event_id = ?';
+        $datatypes = 'i';
+        $vars = array(&$event_id);
+        return $this->mod_data($query, $datatypes, $vars);
     }
 
 }

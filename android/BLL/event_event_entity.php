2c8e4eb2dfe9eeebefb12f8a33872cd345d5fd94
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-12-01 19:14:14 +0200

    update the function name

--- android/BLL/event_event_entity.php
+++ android/BLL/event_event_entity.php
@@ -7,7 +7,7 @@ include_once '../DAL/my_db.php';
 class event_event_entity extends my_db {
 
     //this function get the all the event ids that related to the specified entity
-    function get_event_id($entity_id) {
+    function get_event_ids($entity_id) {
         $query = "SELECT event_id FROM `event_event_entity` WHERE event_entity_id = ?";
         return $this->get_data($query, 'i', array(&$event_id));
     }

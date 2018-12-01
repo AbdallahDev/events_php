345b56aa774d6a308c4cd56d43f1d443cf512f10
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-12-01 19:52:26 +0200

    update the get_events function that get all the events

--- android/BLL/events.php
+++ android/BLL/events.php
@@ -7,7 +7,7 @@ class events extends my_db {
 
     //this function get all the events for all the committees
     function get_events() {
-        $query = "SELECT committees.committee_name, events.event_entity_name, events.subject, events.event_date, events.time FROM committees, events WHERE committees.committee_id = events.committee_id ORDER by events.event_date DESC, events.time DESC";
+        $query = "SELECT events.event_entity_name, events.subject, events.event_date, events.time, events.id FROM events ORDER by events.event_date DESC, events.time DESC";
         return $this->get_all_data($query);
     }
 

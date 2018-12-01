27f5f662fe276262149d9f9f79f1f30055c4ee78
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-12-01 18:14:03 +0200

    declared the entity_id variable

--- android/APIs/get_committee_events.php
+++ android/APIs/get_committee_events.php
@@ -4,10 +4,12 @@
 
 include_once '../BLL/events.php';
 
+//variable declarations
+$entity_id = $_GET['entityId'];
 $events = new events();
 
 $events_array = array();
-$rs_events_android = $events->entity_events_get($_GET['entityId']);
+$rs_events_android = $events->entity_events_get($entity_id);
 while ($row_events_android = $rs_events_android->fetch_assoc()) {
     array_push($events_array, $row_events_android);
 }

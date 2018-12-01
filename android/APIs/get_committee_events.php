c4990372ed38f09f492bf8e40094b5324320f83c
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-12-01 18:47:29 +0200

    organize file space

--- android/APIs/get_committee_events.php
+++ android/APIs/get_committee_events.php
@@ -7,8 +7,8 @@ include_once '../BLL/events.php';
 //variable declarations
 $entity_id = $_GET['entityId'];
 $events = new events();
-
 $events_array = array();
+
 $rs_events_android = $events->entity_events_get($entity_id);
 while ($row_events_android = $rs_events_android->fetch_assoc()) {
     array_push($events_array, $row_events_android);

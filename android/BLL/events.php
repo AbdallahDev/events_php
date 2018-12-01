34c4236d17890e8ddc47d9fe38c32988b643db9f
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-12-01 19:15:46 +0200

    alter the function that get all the events related to a specific entity

--- android/BLL/events.php
+++ android/BLL/events.php
@@ -16,7 +16,7 @@ class events extends my_db {
 
     //this function get all the events for a specific entity
     function entity_events_get($entity_id) {
-        $query = 'SELECT committees.committee_name, events.event_entity_name, events.subject, events.event_date, events.time FROM committees, events WHERE committees.committee_id = events.committee_id AND events.committee_id = ? ORDER by events.event_date DESC, events.time DESC';
+        $query = 'SELECT events.event_entity_name, events.subject, events.event_date, events.time FROM events ORDER by events.event_date DESC, events.time DESC';
         $datatypes = 'i';
         $vars = array(&$entity_id);
         return $this->get_data($query, $datatypes, $vars);

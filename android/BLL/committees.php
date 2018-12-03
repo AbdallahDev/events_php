e23dd2674d26c2a94354a5818d3e12e7d1b0e7ee
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-12-03 10:23:29 +0200

    made the function that get all the committees for a specific category

--- android/BLL/committees.php
+++ android/BLL/committees.php
@@ -13,9 +13,7 @@ class committees extends my_db {
     //here i'll get the committee_id and the committee_name for all the 
     //committees in the database
     function committees_get() {
-        return $this->get_all_data("SELECT committee_id, committee_name FROM "
-                        . "`committees` WHERE `committee_name` != '' "
-                        . "ORDER BY `committees`.`committee_rank`  ASC");
+        return $this->get_all_data("SELECT committee_id, committee_name FROM `committees` WHERE `committee_name` != '' ORDER BY `committees`.`committee_rank` ASC");
     }
 
     //get the entity name depened on the entity id
@@ -23,6 +21,14 @@ class committees extends my_db {
         $query = "SELECT committee_name FROM `committees` WHERE committee_id = ?";
         $datatypes = "i";
         $vars = array(&$entity_id);
+        return $this->get_data($query, $datatypes, $vars);
+    }
+
+    //this function get the entities for a specific category
+    function get_entities_specific_category($category) {
+        $query = "SELECT committee_id, committee_name FROM `committees` WHERE committees.event_entity_category_id = ? ORDER BY `committees`.`committee_rank` ASC";
+        $datatypes = "i";
+        $vars = array(&$category);
         return $this->get_data($query, $datatypes, $vars);
     }
 

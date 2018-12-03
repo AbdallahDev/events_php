63188bc029e78f7c15b1b232c6024cc0e3a51e68
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-12-03 10:27:32 +0200

    updated the file to get all the entities related to a specific entities category

--- android/APIs/get_committees.php
+++ android/APIs/get_committees.php
@@ -1,9 +1,15 @@
 <?php
 
+//this file get all the entities that related to a specific entities category.
+
+//this var store the entities category that provided by the url
+$category = $_GET['category'];
+
 include_once '../BLL/committees.php';
 $committees = new committees();
 $committees_array = array();
-$rs_committees = $committees->committees_get();
+//here i get all the entities related to a specific entity category
+$rs_committees = $committees->get_entities_specific_category($category);
 while ($row_committees = $rs_committees->fetch_assoc()) {
     array_push($committees_array, $row_committees);
 }

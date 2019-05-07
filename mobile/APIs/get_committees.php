48a92732c61dbbf41c64efac98dd684819f7d121
abdallah.sarayrah <abdallah.sarayrah@IT-LAPTOP-ABDAL.JPGOV.GOVER.Local>
2019-05-07 13:07:53 +0300

    I've checked for the categoryId, if it's 0 I'll call the method that gets all the entities to be saved in the mobile local DB, or I'll call the method that gets the entities that belong to a specific category depending on its id.

--- mobile/APIs/get_committees.php
+++ mobile/APIs/get_committees.php
@@ -3,15 +3,21 @@
 //this file will get the entities (eg. committees) from the phpmyadmin db
 //i'll send them to mobile app to be saved in the local db.
 //this file get all the entities (committees) that related to a specific entities category.
-
 //this var store the entities category that provided by the url
-$category = $_GET['category'];
+$categoryId = $_GET['categoryId'];
 
 include_once '../BLL/committees.php';
 $committees = new committees();
 $committees_array = array();
+//I'll check the categoryId, if it's 0 I'll get all the entities to be sent to the mobile app to be saved in the local db, else I'll get
+//the entities that belong to a specific category.
+if ($categoryId == 0) {
+    $rs_committees = $committees->committees_get();
+}
 //here i get all the entities related to a specific entity category
-$rs_committees = $committees->get_entities_specific_category($category);
+else {
+    $rs_committees = $committees->get_entities_specific_category($categoryId);
+}
 while ($row_committees = $rs_committees->fetch_assoc()) {
     array_push($committees_array, $row_committees);
 }

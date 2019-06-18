37122436875177a6015cbfe4a94d572fb1307afb
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-06-18 11:51:25 +0300

    I've modified the event that selects the entity IDs for the inserted events, by selecting also the event data and time to be able to sort them based on those two fields.

--- mobile/BLL/event_event_entity.php
+++ mobile/BLL/event_event_entity.php
@@ -8,9 +8,14 @@ include_once '../DAL/my_db.php';
 //and that for the phone app
 class event_event_entity extends my_db {
 
-    //this function get the all the event ids that related to the specified entity
+    //This function gets the all the event IDs that related to the specified entity, 
+    //and also I've selected the event date and time, because If I don't do that 
+    //the selected events will be shown in the app as the latest event shown in 
+    //the last and the old one in the first.
     function get_event_id($entity_id) {
-        $query = "SELECT event_id FROM `event_event_entity` WHERE event_entity_id = ?";
+        $query = "SELECT event_id, events.event_date, events.time "
+                . "FROM `event_event_entity` INNER JOIN events ON event_event_entity.event_id = events.id "
+                . "WHERE event_entity_id = ? ORDER BY events.event_date DESC, events.time DESC";
         return $this->get_data($query, 'i', array(&$entity_id));
     }
 

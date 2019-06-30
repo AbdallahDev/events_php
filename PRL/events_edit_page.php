d5568b7b52955834071ec50af70fe829d1300b55
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-06-30 12:44:16 +0300

    I've deleted an unneeded console function.

--- PRL/events_edit_page.php
+++ PRL/events_edit_page.php
@@ -87,7 +87,6 @@ elseif ($event_event_entity_rs->num_rows >= 1 && $events_row['event_entity_name'
                             //js variable to use it for selecting the entity in 
                             //the entities drop down menu.
                             var event_entity_id = <?php echo $event_entity_id; ?>;
-                            console.log(entity_id);
                             $("#committee").append('<option value="' + entities.committee_id + '">' + entities.committee_name + '</option>')
                         })
                     } else {

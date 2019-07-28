0a3176533648550445bdf052537ce46fb18ac9c7
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-07-28 13:59:41 +0300

    I've called the function that inserts a new entry for the specified identifier with a new token.

--- mobile/APIs/save_device_token.php
+++ mobile/APIs/save_device_token.php
@@ -17,6 +17,20 @@ $device_token_obj = new device_token();
 //Here I'll call the function that checks if the identifier exists.
 $identifier_check_rs = $device_token_obj->check_identifier($device_identifier);
 
+//Here I'll check if the result after calling the function that checks for the 
+//device identifier has data by checking for the row numbers.
+if ($device_identifier_check_rs->num_rows > 0) {
+    //Here I'll call the function that updates the token for the specified 
+    //identifier because the number of rows is more than zero and that means the 
+    //identifier already exists.
+    $device_token_obj->update_token($device_token, $device_identifier);
+} else {
+    //Here I'll call the function that inserts a new entry for the specified 
+    //identifier with a new token, and that because the number of rows is less 
+    //than zero and that means the specified identifier does not exist in the DB.
+    $device_token_obj->store_device_token($device_token, $device_identifier);
+}
+
 //Here I'll delete all the duplicate device tokens from the DB, and that based 
 //on the device identifier.
 $device_token_obj->delete_duplicated_tokens($device_identifier);

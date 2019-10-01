de0b20bb619c8065e96a7df5c7ed36328a71e424
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-10-01 13:37:26 +0300

    I've selected the event appointment for all the events when all the categories are selected.

--- mobile/BLL/events.php
+++ mobile/BLL/events.php
@@ -20,6 +20,10 @@ class events extends my_db {
                 //below I'll format the selected time to make it appears in the 
                 //mobile app without seconds.
                 . "DATE_FORMAT(events.time, '%H:%i') AS `time`, "
+                //I've selected the event appointment because the event 
+                //sometimes has a time filled in the appointment text box in the 
+                //events web system, so I need to show it in the mobile app.
+                . "events.event_appointment, "
                 . "halls.hall_name, event_place "
                 . "FROM events "
                 . "INNER JOIN halls ON events.hall_id = halls.hall_id "

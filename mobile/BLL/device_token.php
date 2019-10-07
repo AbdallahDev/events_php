8d35cf7c66ab27472fae08f969a6588d8a198c04
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-10-07 09:45:56 +0300

    I've formatted the code.

--- mobile/BLL/device_token.php
+++ mobile/BLL/device_token.php
@@ -12,30 +12,34 @@ class device_token extends my_db {
     //tokens at first because that will give them the priority to receive the 
     //message early.
     public function get_all_device_token() {
-        return $this->get_all_data('SELECT device_token FROM `device_token` ORDER BY dateTime DESC');
+        return $this->get_all_data('SELECT device_token FROM `device_token` '
+                        . 'ORDER BY dateTime DESC');
     }
 
     //This function will check for the device identifier existence, if its exist 
     //I'll update the token connected with it, and if its not I'll insert a new 
     //entry for that identifier with a new entry.
     function check_identifier($device_identifier) {
-        $query = "SELECT device_token_id FROM `device_token` WHERE device_identifier = ?";
+        $query = "SELECT device_token_id FROM `device_token` "
+                . "WHERE device_identifier = ?";
         return $this->get_data($query, "s", array(&$device_identifier));
     }
 
     //This function will update the current token with a new one for the 
     //specified device identifier.
     function update_token($device_token, $device_identifier) {
-        $query = "UPDATE `device_token` SET `device_token`= ?,`dateTime`= CURRENT_TIMESTAMP "
+        $query = "UPDATE `device_token` SET `device_token`= ?, "
+                . "`dateTime`= CURRENT_TIMESTAMP "
                 . "WHERE device_identifier = ?";
-        return $this->mod_data($query, "ss", array(&$device_token, &$device_identifier));
+        return $this->mod_data($query, "ss", array(&$device_token
+                    , &$device_identifier));
     }
 
     //this function will delete all the duplicated tokens and that based on the 
     //device identifier.
     //Because if there is duplication in the DB the same message will be 
     //sent multiple times for the same device.
-    function delete_duplicated_tokens($device_identifier) {
+    function delete_duplicated_tokens($device_name, $device_model) {
         $this->mod_data('DELETE FROM `device_token` WHERE device_identifier = ?'
                 , 's', array(&$device_identifier));
     }
@@ -51,7 +55,8 @@ class device_token extends my_db {
          * device_model and device_isPhysical in the DB, and i'll get the values 
          * from the url */
         $query = "INSERT INTO `device_token`(`device_token`, `device_identifier`, "
-                . "`device_name`, `device_model`, `device_isPhysical`) VALUES (?,?,?,?,?)";
+                . "`device_name`, `device_model`, `device_isPhysical`) "
+                . "VALUES (?,?,?,?,?)";
         $this->mod_data($query, 'sssss', array(&$device_token, &$device_identifier,
             &$device_name, &$device_model, &$device_isPhysical));
     }

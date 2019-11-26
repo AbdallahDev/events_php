fdb5b4bc1e378f2cf256fbcc8bd63232456bebc6
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-11-26 14:57:17 +0200

    I've created the array that stores the data related to the devices that have IOS.

--- mobile/APIs/event_notification.php
+++ mobile/APIs/event_notification.php
@@ -46,6 +46,9 @@ $rs_device_token = $device_token->get_all_device_token();
 //This array declaration is to store the devices tokens to send them at once to 
 //the FCM sending function, instead of sending each token alone.
 $registration_ids = array();
+//This array will store the data like(device_token, device_identifier, 
+//badge_count) that related to the devices that has IOS.
+$registration_ids_ios = array();
 //Below I'll loop over the device tokens to store them in the registration_ids 
 //array.
 while ($row_device_token = $rs_device_token->fetch_assoc()) {

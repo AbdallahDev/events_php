45e1427f354fc9372f2974a1b2a5c6bae713d607
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-11-28 13:27:58 +0200

    I've changed the instance name to be more expressive.

--- mobile/APIs/event_notification.php
+++ mobile/APIs/event_notification.php
@@ -33,17 +33,17 @@ if (empty(trim($event_entity_name))) {
 define('API_ACCESS_KEY'
         , 'AAAAeotQvx8:APA91bF0Llvsw2XqmQ4IW-HJMEEgVriiBO2qbKIsrdZt2EKN2Lq66Vec2V9faJi89gQkoN4FBd6_jynTc3vPm8TFrYcW_NhopsDBFJvbkcuWv16G2-hj2_Nsa-qrof0FmShfYN1A9L79');
 
-//bellow i'll select all the device tokens in the db to send them notifications
-
+//Below I'll select all the devices data from the DB to send them notifications.
+//
 include_once '../mobile/BLL/device_token.php';
 $device_token = new device_token();
-$rs_device_token = $device_token->get_all_device_token();
+$rs_devices_data = $device_token->get_all_device_token();
 //This array declaration is to store the devices tokens to send them at once to 
 //the FCM sending function, instead of sending each token alone.
 $registration_ids = array();
 //Below I'll loop over the device tokens to store them in the registration_ids 
 //array.
-while ($row_device_token = $rs_device_token->fetch_assoc()) {
+while ($row_device_token = $rs_devices_data->fetch_assoc()) {
     $registration_ids[] = $row_device_token['device_token'];
 }
 

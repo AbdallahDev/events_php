02422ffcdf4dcec147226b2adbf13b6e54a427e5
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-12-29 14:06:25 +0200

    I've created the function that will get the hall name to be sent with the app event notification.

--- BLL/halls.php
+++ BLL/halls.php
@@ -8,4 +8,11 @@ class halls extends my_db {
         return $this->get_all_data('select * from halls');
     }
 
+    //This function will get the hall name when it's chosen from the dropdown 
+    //menu for the event, and it will be sent with the app notification.
+    public function hall_name_get($hall_id) {
+        return $this->get_data('SELECT `hall_name` FROM `halls` WHERE `hall_id` = ?'
+                        , 'i', array(&$hall_id));
+    }
+
 }
